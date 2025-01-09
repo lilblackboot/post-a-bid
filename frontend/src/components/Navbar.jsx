5a7080d591c07efccbadda13627b6f6ca3538301
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2025-01-10 00:41:00 +0530

    nav bar done

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='bg-[#262C35] flex justify-between align-middle items-center'>
+            <h1 className='text-white text-4xl p-4'>POST-A-BID</h1>
+            <div className='flex justify-between gap-2'>
+                <div className='bg-[#FE8D32] text-white p-2 px-5 rounded-full m-3'>Login</div>
+                <div className='bg-[#367BFE] text-white p-2 px-5 rounded-full m-3'>Signup</div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
