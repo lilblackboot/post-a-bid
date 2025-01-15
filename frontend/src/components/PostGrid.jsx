3799ed50b6893f22f6a4308ecce815f4429d7253
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2025-01-15 13:10:37 +0530

    done?

--- frontend/src/components/PostGrid.jsx
+++ frontend/src/components/PostGrid.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const posts = [
   { id: 1,  user: {
-      name: 'Jane Smith',
+      name: 'Suzan Mansuri',
       profilePicture: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
     }, image: 'https://media.istockphoto.com/id/175499936/photo/isolated-shot-of-wooden-table-on-white-background.jpg?s=612x612&w=0&k=20&c=ehujtyZnSnXVxlpzfuLwHHqMJgPJzfpLtzcmrjHsecA=', title: 'Antique Table' ,disc : 'this is a antique table',timeleft : '2 hours left',currentbid : '25,000'},
   { id: 2,  user: {
-      name: 'Jane Smith',
+      name: 'Swapnendu',
       profilePicture: 'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg',
     }, image: 'https://assets.ajio.com/medias/sys_master/root/20230508/ivQe/6458f8e5d55b7d0c639c2120/-473Wx593H-466135523-pink-MODEL.jpg', title: 'Vintage Carpet',disc : 'this is a antique table',timeleft : '2 days left',currentbid : '7,00,000'} ,
   { id: 3,  user: {
-      name: 'Jane Smith',
+      name: 'Yash Chauhan',
       profilePicture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
     }, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6r0AZeVloAlogqFtOXnNFE2Xb-82JdXqZKg&s', title: 'cutural vas',disc : 'this is a antique table',timeleft : '3 minutes left',currentbid : '10,000' },
   // Add more posts...
