85a06c0587925533d6a0a2251219ee96d3f6ea25
G0D-SLAYER <159141717+G0D-SLAYER@users.noreply.github.com>
2025-01-17 21:27:10 +0530

    merged to abhinav branch to main and made changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
 import { UserProvider } from './UserContext'; // Ensure this import is correct
+import './App.css'
+import Navbar from './components/Navbar'
+import SearchBar from './components/SearchBar'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import OtpVerification from './pages/OtpVerification';
+import Sidebar from './components/SideBar'  
 
 function App() {
     return (
@@ -19,31 +23,6 @@ function App() {
             </BrowserRouter>
         </UserProvider>
     );
-=======
-import { useState } from 'react'
-import{BrowserRouter ,Routes,Route} from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import SearchBar from './components/SearchBar'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import Sidebar from './components/SideBar'  
-function App() {
-
-  return (
-    <BrowserRouter>
-      <div> 
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-        </Routes>
-       </div>
-    </BrowserRouter>
-  )
->>>>>>> 36c531b66b17bd39ccf25b49ceed50b440808751
 }
 
 export default App;
