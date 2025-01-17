36c531b66b17bd39ccf25b49ceed50b440808751
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2025-01-17 15:57:58 +0530

    sidebar cleanup

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -10,7 +10,7 @@ const Sidebar = ({ onToggle }) => {
 
   return (
     <div
-      className={`h-screen bg-gray-800 text-white ${
+      className={`h-screen bg-[#262C35] text-white ${
         isCollapsed ? "w-16" : "w-64"
       } fixed transition-all duration-300`}
     >
@@ -23,15 +23,25 @@ const Sidebar = ({ onToggle }) => {
       </button>
 
       {/* Sidebar Content */}
-      <div className={`p-4 ${isCollapsed ? "hidden" : ""}`}>
-        <h1 className="text-lg font-bold border-b border-gray-700 mb-4">
-          Sidebar Title
-        </h1>
-        <ul className="space-y-2">
-          <li className="p-2 hover:bg-gray-700 cursor-pointer">Home</li>
-          <li className="p-2 hover:bg-gray-700 cursor-pointer">Profile</li>
-          <li className="p-2 hover:bg-gray-700 cursor-pointer">Settings</li>
-          <li className="p-2 hover:bg-gray-700 cursor-pointer">Logout</li>
+      <div className={`p-4 ${isCollapsed ? "hidden" : ""} bg-transparent`}>
+
+      <div className="flex p-3 rounded-2xl bg-[#D9D9D9] items-center mb-2">
+              <img
+                src='https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8'
+                alt='alt'
+                className="w-16 h-16 rounded-full object-cover mr-3"
+              />
+              <div className="bg-transparent">  
+                <p className="text-lg bg-transparent font-semibold text-gray-800">Suzan Mansuri</p>
+            
+              </div>
+            </div>
+        
+        <ul className="bg-transparent space-y-2">
+          <li className="p-2 hover:bg-gray-700 bg-transparent cursor-pointer">Home</li>
+          <li className="p-2 hover:bg-gray-700 bg-transparent cursor-pointer">Profile</li>
+          <li className="p-2 hover:bg-gray-700 bg-transparent cursor-pointer">Settings</li>
+          <li className="p-2 hover:bg-gray-700 bg-transparent cursor-pointer">Logout</li>
         </ul>
       </div>
     </div>
